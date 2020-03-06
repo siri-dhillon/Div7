5731368b6dbf96dd37fd0d3c092ed334ead63cce
Eddie-Z <35039865+Eddie-Z@users.noreply.github.com>
2020-03-05 20:36:19 -0800

    Add files via upload

--- lab3_350div7/Source/Div7.vhd
+++ lab3_350div7/Source/Div7.vhd
@@ -3,7 +3,7 @@ Use ieee.std_logic_1164.all;
 Use ieee.numeric_std.all;
 
 Entity Div7 is
-Generic ( N : natural := 6 );
+Generic ( N : natural := 18 );
 Port ( x : in std_logic_vector( N-1 downto 0 );
 	IsDivisible : out std_logic );
 End Entity Div7;
@@ -36,19 +36,17 @@ Begin
 
 -- Insert your design here -------------------------------------------------------------------------------------
 
---level 0
-								--s1=carry S0= sum  
-	FA_A1: entity work.FullAdder(rtl) port map (A =>, B =>, C =>, 			S1=>CA1_B1, S0=> SA1_B0);
-	FA_A2: entity work.FullAdder(rtl) port map (A =>, B =>, C =>, 			S1=>CA2_G1, S0=> SA2_G0);
-	FA_A3: entity work.FullAdder(rtl) port map (A =>, B =>, C =>, 			S1=>CA3_G2, S0=> SA3_B2);
-	FA_A4: entity work.FullAdder(rtl) port map (A =>, B =>, C =>, 			S1=>CA4_B1, S0=> SA4_B0);
-	FA_A5: entity work.FullAdder(rtl) port map (A =>, B =>, C =>, 			S1=>CA5_B2, S0=> SA5_B1);
-	FA_A6: entity work.FullAdder(rtl) port map (A =>, B =>, C =>, 			S1=>CA6_G2, S0=> SA6_B2);
+--level 0							--s1=carry S0= sum  
+	FA_A1: entity work.FullAdder(rtl) port map (A =>x(0),  B =>x(1),  C =>x(2), 	S1=>CA1_B1, S0=> SA1_B0);
+	FA_A2: entity work.FullAdder(rtl) port map (A =>x(3),  B =>x(4),  C =>x(5), 	S1=>CA2_G1, S0=> SA2_G0);
+	FA_A3: entity work.FullAdder(rtl) port map (A =>x(6),  B =>x(7),  C =>x(8), 	S1=>CA3_G2, S0=> SA3_B2);
+	FA_A4: entity work.FullAdder(rtl) port map (A =>x(9),  B =>x(10), C =>x(11), 	S1=>CA4_B1, S0=> SA4_B0);
+	FA_A5: entity work.FullAdder(rtl) port map (A =>x(12), B =>x(13), C =>x(14),   S1=>CA5_B2, S0=> SA5_B1);
+	FA_A6: entity work.FullAdder(rtl) port map (A =>x(15), B =>x(16), C =>x(17),   S1=>CA6_G2, S0=> SA6_B2);
 --level 1								--s1=carry S0= sum 
 	HA_B0: entity work.FullAdder(rtl) port map (A =>SA1_B0, B =>SA4_B0, C =>0, 	S1=>CB0_G0, S0=>output(0));
 	FA_B1: entity work.FullAdder(rtl) port map (A =>SA5_B1, B =>CA1_B1, C =>CA4_B1, S1=>CB1_G1, S0=> SB1_G0);
 	FA_B2: entity work.FullAdder(rtl) port map (A =>SA3_B2, B =>CA5_B2, C =>SA6_B2, S1=>CB2_G2, S0=> SB2_G1);
-
 --level 2
 	FA_G0: entity work.FullAdder(rtl) port map (A =>SA2_G0, B =>SB1_G0, C =>CB0_G0, S1=>CG0_D0 , S0=>output(1));
 	FA_G1: entity work.FullAdder(rtl) port map (A =>CA2_G1, B =>SB2_G1, C =>CB1_G1, S1=>CG1_D1 , S0=>SG1_D0);
@@ -60,6 +58,18 @@ Begin
 	HA_T0: entity work.FullAdder(rtl) port map (A =>CD0_T0, B =>SD1_T0, C =>0, 	S1=>CT0_W0, S0=>output(3));
 --level 5
 	HA_W0: entity work.FullAdder(rtl) port map (A =>CT0_W0, B =>CD1_W0, C =>CG2_W0, S1=>output(5), S0=>output(4));
+	
+--checking if output is /63//14/7/0
+
+if (X = 5)  then
+	Z <= A;
+elsif (X >= 5) then
+	Z <= B;
+else
+	Z < C;
+end if;
+
+	
 	
 End Architecture structural;
 
